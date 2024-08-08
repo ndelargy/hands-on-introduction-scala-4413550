c307be9d5a3eb7a21b2c2bdcdbda171e433f742b
ndelargy <neil.delargy@gmail.com>
2024-08-08 15:23:34 +0100

    chapter 2_1 complete

--- src/test/scala/EtlSpec.scala
+++ src/test/scala/EtlSpec.scala
@@ -1,8 +1,10 @@
+import Etl.etl
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
+
 import scala.util.Using
 import scala.io.Source
-import scala.util.{Try, Success}
+import scala.util.{Success, Try}
 
 class EtlSpec extends AnyFreeSpec with Matchers {
 
@@ -14,7 +16,21 @@ class EtlSpec extends AnyFreeSpec with Matchers {
         "src/test/resources/testOutput.txt"
       val expectedFileContents = List("hello world")
 
-      etl(input, output)
+      etl(input, output)(using Etl.StringImpl)
+
+      readFile(output) shouldEqual Success(expectedFileContents)
+    }
+  }
+
+  "etl" - {
+    "transforms a text file by doubling all integers saves it to a new file" in {
+      val input =
+        "src/test/resources/testInput2.txt"
+      val output =
+        "src/test/resources/testOutput2.txt"
+      val expectedFileContents = List("4", "8", "12", "16", "2", "6", "10")
+
+      etl(input, output)(using Etl.IntImpl)
 
       readFile(output) shouldEqual Success(expectedFileContents)
     }
